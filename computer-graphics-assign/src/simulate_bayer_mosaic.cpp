afeecad8db8de1e652e60159fdeb1199a3f802de
Ratnajagadeesharava <arj15@iitbbs.ac.in>
2024-01-04 21:51:50 +0530

    adde bayer mosaic filter and samples for testing

--- computer-graphics-assign/src/simulate_bayer_mosaic.cpp
+++ computer-graphics-assign/src/simulate_bayer_mosaic.cpp
@@ -1,5 +1,6 @@
 #include "simulate_bayer_mosaic.h"
-
+#include <iostream>
+using namespace std;
 void simulate_bayer_mosaic(
   const std::vector<unsigned char> & rgb,
   const int & width,
@@ -9,5 +10,40 @@ void simulate_bayer_mosaic(
   bayer.resize(width*height);
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
+  // GRGBGRBGRBGRGBGRGBGRGBGRGBG
   ////////////////////////////////////////////////////////////////////////////
+
+  //temp storage with vector of rgb
+
+  vector<vector<unsigned char>> tempStorage;
+  tempStorage.resize(width * height);
+  int c = 0;
+  for (int i = 0; i < height * width * 3; i=i+3) {
+	  vector<unsigned char> rgb3d;
+	  rgb3d.resize(3);
+	  rgb3d[0] = rgb[i];
+	  rgb3d[1] = rgb[i+1];
+	  rgb3d[2] = rgb[i+2];
+	  tempStorage[c] = rgb3d;
+	  c++;
+  }
+  bool flag = false;
+  for (int i = 0; i < height*width; i++) {
+	  if (!flag) {
+		  bayer[i] = tempStorage[i][1];
+		}
+	  else {
+		  int k = i / width;
+		  if (k % 2 == 0) {
+			  bayer[i] = tempStorage[i][2];
+		  }
+		  else {
+			  bayer[i] = tempStorage[i][0];
+		  }
+
+	  }
+	  flag = !flag;
+  }
+
+
 }
